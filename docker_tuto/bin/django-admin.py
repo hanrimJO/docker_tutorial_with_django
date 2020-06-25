121e77b098017048ffcb42c0790005fc3f749614
hanrimJO <whgksfla@gmail.com>
2020-06-25 20:46:59 +0900

    Apply gitignore

--- docker_tuto/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/hanrimjo/workspaces/docker_tutorial_with_django/docker_tuto/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
