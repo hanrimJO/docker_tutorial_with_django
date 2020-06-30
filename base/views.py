577208d6dbbaa1d5bf9141e370c1fc3773d579ad
hanrimJO <whgksfla@gmail.com>
2020-06-30 14:57:37 +0900

    Test jenkins auto build

--- base/views.py
+++ base/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('jenkins auto build third test')
+    return HttpResponse('jenkins auto build 4 test')
